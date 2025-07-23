b26469c8d5c9945e22b77caca0f49f3b5a93d27d
bysum <ssuddie0726@naver.com>
2025-07-23 17:12:40 +0900

    refactor: 회원 기능 도메인/리포지토리/서비스 구성

--- src/main/java/com/byunsum/ticket_reservation/member/repository/MemoryMemberRepository.java
+++ src/main/java/com/byunsum/ticket_reservation/member/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.byunsum.ticket_reservation.member.repository;
 
 import com.byunsum.ticket_reservation.member.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
