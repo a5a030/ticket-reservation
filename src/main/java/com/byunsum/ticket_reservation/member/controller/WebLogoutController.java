e33d9b5c8775f6df1026d9adc490b583cd38f0e7
bysum <ssuddie0726@naver.com>
2025-07-24 15:09:15 +0900

    feat: 로그아웃까지 연결

---
+++ src/main/java/com/byunsum/ticket_reservation/member/controller/WebLogoutController.java
@@ -0,0 +1,18 @@
+package com.byunsum.ticket_reservation.member.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class WebLogoutController {
+    @GetMapping("/web/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/web/login-form?logout";
+    }
+}
