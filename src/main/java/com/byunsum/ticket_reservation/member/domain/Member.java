befa2298c166ada5d29eb0c062f4e6b3298875f2
bysum <ssuddie0726@naver.com>
2025-07-25 09:30:27 +0900

    fix: 회원가입 에러 수정

--- src/main/java/com/byunsum/ticket_reservation/member/domain/Member.java
+++ src/main/java/com/byunsum/ticket_reservation/member/domain/Member.java
@@ -1,16 +1,14 @@
 package com.byunsum.ticket_reservation.member.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "member")
 public class Member {
     @Id
     @GeneratedValue
-    private  String name;
     private Long id;
+    private  String name;
     private String password;
 
     public String getName() {
