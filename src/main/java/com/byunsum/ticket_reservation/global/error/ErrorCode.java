cde702869d283e0a0b43fb5ba6c62edaf8ae9498
bysum <ssuddie0726@naver.com>
2025-07-29 12:27:09 +0900

    feat: 좌석 예외 코드 SEAT_ALREADY_SELECTED, SEAT_ALREADY_RESERVED 추가

--- src/main/java/com/byunsum/ticket_reservation/global/error/ErrorCode.java
+++ src/main/java/com/byunsum/ticket_reservation/global/error/ErrorCode.java
@@ -11,9 +11,12 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND("회원을 찾을 수 없습니다."),
     DUPLICATE_MEMBER("이미 존재하는 회원입니다."),
     INVALID_PASSWORD("비밀번호가 일치하지 않습니다."),
+
     PERFORMANCE_NOT_FOUND("공연 정보를 찾을 수 없습니다."),
     RESERVATION_NOT_FOUND("예매 정보를 찾을 수 없습니다."),
-    DUPLICATE_SEAT("이미 예매된 좌석입니다."),
+    SEAT_ALREADY_RESERVED("이미 예매된 좌석입니다."),
+    SEAT_ALREADY_SELECTED("이미 선택된 좌석입니다."),
+
     NOTIFICATION_NOT_FOUND("알림을 찾을 수 없습니다.");
 
     private final String message;
