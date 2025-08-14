0d531a75b4938d36ff8a3e78f1790357e671afa1
bysum <ssuddie0726@naver.com>
2025-08-14 11:28:36 +0900

    feat: Redis 기본 설정 추가

--- src/main/java/com/byunsum/ticket_reservation/global/config/RedisConfig.java
+++ src/main/java/com/byunsum/ticket_reservation/global/config/RedisConfig.java
@@ -1,17 +1,37 @@
 package com.byunsum.ticket_reservation.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.username}")
+    private String redisUsername;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setUsername(redisUsername);
+        config.setPassword(redisPassword);
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
