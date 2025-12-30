cbd43eff46f089b709cb951e2572de11651b4ae3
bysum <ssuddie0726@naver.com>
2025-12-31 00:18:44 +0900

    refactor: RedisConfig에서 수동 Lettuce 설정 제거하고 자동구성 사용

--- src/main/java/com/byunsum/ticket_reservation/global/config/RedisConfig.java
+++ src/main/java/com/byunsum/ticket_reservation/global/config/RedisConfig.java
@@ -11,39 +11,40 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-
-    @Value("${spring.data.redis.username}")
-    private String redisUsername;
-
-    @Value("${spring.data.redis.password}")
-    private String redisPassword;
+//    @Value("${spring.data.redis.host}")
+//    private String redisHost;
+//
+//    @Value("${spring.data.redis.port}")
+//    private int redisPort;
+//
+//    @Value("${spring.data.redis.username}")
+//    private String redisUsername;
+//
+//    @Value("${spring.data.redis.password}")
+//    private String redisPassword;
+//
+//    @Bean
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+//        config.setHostName(redisHost);
+//        config.setPort(redisPort);
+//        config.setUsername(redisUsername);
+//        config.setPassword(redisPassword);
+//
+//        return new LettuceConnectionFactory(config);
+//    }
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(redisHost);
-        config.setPort(redisPort);
-        config.setUsername(redisUsername);
-        config.setPassword(redisPassword);
-
-        return new LettuceConnectionFactory(config);
-    }
-
-    @Bean
-    public StringRedisTemplate redisTemplate(RedisConnectionFactory factory) {
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
         return new StringRedisTemplate(factory);
     }
 
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory factory) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(factory);
-
-        return container;
-    }
+    // 만료이벤트
+//    @Bean
+//    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory factory) {
+//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+//        container.setConnectionFactory(factory);
+//
+//        return container;
+//    }
 }
